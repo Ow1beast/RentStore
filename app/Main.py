922be2743f1d8a8cb8f5c4d24f17e7595b124a2d
Alibek <alibek.shokanov@gmail.com>
2025-04-07 00:20:29 +0500

    Comm #12

--- app/Main.py
+++ app/Main.py
@@ -2,19 +2,20 @@ from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from datetime import timedelta
+from fastapi.staticfiles import StaticFiles
 from . import auth, crud, schemas, models
 from .deps import get_db
 from .database import engine
-from fastapi.staticfiles import StaticFiles
-
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
 @app.post("/users/")
 def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db, user)
+
+@app.post("/token")
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = crud.get_user(db, form_data.username)
     if not user or not auth.verify_password(form_data.password, user.password_hash):
@@ -25,3 +26,6 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
 @app.get("/users/me")
 def read_users_me(current_user: models.User = Depends(auth.get_current_user)):
     return current_user
+
+# 👇 СТАВИМ В САМЫЙ КОНЕЦ
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
